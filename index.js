57ef518b881d13a1e598d99f9df52053c456bc3a
Diego Ortiz <dxortiz2@gmail.com>
2021-12-07 10:02:02 -0600

    mysql connection

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyparser = require('body-parser')
+
+const app = express();
+app.use(bodyparser.json());
+
+// config mysql connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'node.user',
+    password: 'root',
+    database: 'learner',
+    multipleStatements: true
+});
+
+// connect to mysql
+db.connect(err => {
+    if (err) {
+        throw err
+    }
+    console.log('MySQL Connected')
+});
+
+//Creating GET Router to fetch all the learner details from the MySQL Database
+app.get('/learner', (req, res) => {
+    db.query('SELECT * FROM learnerdetails', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Router to GET specific learner detail from the MySQL database
+app.get('/learner/:id', (req, res) => {
+    db.query(`SELECT * FROM learnerdetails WHERE learner_id = ${req.params.id}`, (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+})
