7b0ede72f86288c41c7ed384774c0c6ce51a06b7
Diego Ortiz <dxortiz2@gmail.com>
2021-12-07 14:19:49 -0600

    creating crud routes

--- index.js
+++ index.js
@@ -42,6 +42,43 @@ app.get('/learner/:id', (req, res) => {
     })
 });
 
+//Router to INSERT/POST a learner's detail
+app.post('/learner', (req, res) => {
+    let learner = req.body;
+    var sql = 
+        `SET @learner_id = ?;
+         SET @learner_name = ?;
+         SET @learner_email = ?;
+         SET @course_Id = ?; 
+         CALL learnerAddOrEdit(@learner_id, @learner_name, @learner_email, @course_Id);`;
+    db.query(sql, [learner.learner_id, learner.learner_name, learner.learner_email, learner.course_Id], (err, rows, fields) => {
+    if (!err)
+    rows.forEach(element => {
+    if(element.constructor == Array)
+    res.send('New Learner ID : '+ element[0].learner_id);
+    });
+    else
+    console.log(err);
+    })
+    });
+
+//Router to UPDATE a learner's detail
+app.put('/learner', (req, res) => {
+    let learner = req.body;
+    var sql = 
+        `SET @learner_id = ?;
+         SET @learner_name = ?;
+         SET @learner_email = ?;
+         SET @course_Id = ?; 
+         CALL learnerAddOrEdit(@learner_id, @learner_name, @learner_email, @course_Id);`;
+    db.query(sql, [learner.learner_id, learner.learner_name, learner.learner_email, learner.course_Id], (err, rows, fields) => {
+    if (!err)
+    res.send('Learner Details Updated Successfully');
+    else
+    console.log(err);
+    })
+    });
+    
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
